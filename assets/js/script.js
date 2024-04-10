f2a9afa6c24ccd0046479617d23fc4d9bd8ae511
Sophie Carr <sacarr98@gmail.com>
2024-04-10 12:49:34 +0000

    add event listener to log coordinates of slot clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,8 @@ var tableSlot = document.querySelector(".game-slot");
 const playersTurn = document.querySelector(".players-turn");
 const resetButton = document.querySelector(".reset-game");
 
+for(let i = 0; i< slotCell.length; i++) {
+    slotCell[i].addEventListener('click', (e) => {
+        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
+    })
+}
