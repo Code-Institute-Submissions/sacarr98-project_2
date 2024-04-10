ee5385bf07accaf4e1e572b93c5cde749f1efd68
Sophie Carr <sacarr98@gmail.com>
2024-04-10 12:32:42 +0000

    assign query selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 // selectors
 
 var slotRow = document.getElementsByTagName("tr");
+var slotCell = document.getElementsByTagName("td");
+var tableSlot = document.querySelector(".game-slot");
+const playersTurn = document.querySelector(".players-turn");
+const resetButton = document.querySelector(".reset-game");
+
