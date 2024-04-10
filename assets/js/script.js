45f0b24389dd97e92e1b48b5069f3202cd59986c
Sophie Carr <sacarr98@gmail.com>
2024-04-10 13:04:57 +0000

    create colour change function to change cell colour clicked by player

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,49 @@ var tableSlot = document.querySelector(".game-slot");
 const playersTurn = document.querySelector(".players-turn");
 const resetButton = document.querySelector(".reset-game");
 
+// identify cell coordinates clicked
 for(let i = 0; i< slotCell.length; i++) {
     slotCell[i].addEventListener('click', (e) => {
         console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
     })
 }
 
+// prompt users to enter their player name
 while(!player1){
-    var player1 = promt('Player One (red counter): Enter your name');
+    var player1 = prompt('Player One (red counter): Enter your name');
 }
 
 player1Colour = 'red';
 
 while(!player2){
-    var player2 = promt('Player Two (yellow counter): Enter your name');
+    var player2 = prompt('Player Two (yellow counter): Enter your name');
 }
 
 player2Colour = 'yellow';
+
+var currentPlayer = 1;
+playersTurn.textContent = `${player1}'s make your move`;
+
+Array.prototype.forEach.call(slotCell, (cell)=>{
+    cell.addEventListener('click', colourChange);
+    // ensure background colour is white (empty cell)
+    cell.style.backgroundColor = 'white';
+})
+
+// change colour of cell clicked by player
+function colourChange(e){
+    let column = e.target.cellIndex;
+    let row = [];
+    // loop through cells starting at the bottom row (index 5)
+    for(let i = 5; i > -1; i--){
+        // check cell is currently empty
+        if(slotRow[i].children[column].style.backgroundColor == 'white'){
+            // add selected cell to row array
+            row.push(slotRow[i].children[column]);
+            // change colour of cell depending on which player clicked
+            if(currentPlayer === 1){
+                row[0].style.backgroundColor = player1Colour;
+            }
+        }
+    }
+}
